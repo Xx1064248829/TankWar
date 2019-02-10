c92bbd6920720ad8f19265361dfd79c16280365f
Xx1064248829 <1064248829@qq.com>
2019-02-10 16:31:12 +0800

    创建一个线程，实现自己坦克的运动。（这里并没有进行运动）

--- src/com/ecut/TankClient.java
+++ src/com/ecut/TankClient.java
@@ -1,9 +1,20 @@
 package com.ecut;
 
-import  java.awt.*;
+import java.awt.*;
 import java.awt.event.*;
 
 public class TankClient extends Frame{
+
+    int x=50,y=50;
+
+    public void paint(Graphics g) {            //重写Paint方法画出自己的坦克
+        Color c = g.getColor();
+        g.setColor(Color.RED);
+        g.fillOval(50,50,30,30);
+        g.setColor(c);
+
+        y += 5;
+    }
 
     public void lauchFrame(){
         this.setLocation(400,300);//设置窗口位置
@@ -15,11 +26,30 @@ public class TankClient extends Frame{
             }
         });
         this.setResizable(false);    //设置不能随意改变窗口大小
+        this.setBackground(Color.GREEN);   //窗口背景颜色
         setVisible(true);
-    }
 
+        new Thread(new PaintThread()).start();
+    }
+    //////////////////////////////////////////主方法///////////////////////////////////////////////
     public static void main(String[] args){
         TankClient tc=new TankClient();
         tc.lauchFrame();
     }
+
+    private class PaintThread implements Runnable{
+        public void run() {
+            while(true){
+                repaint();           //调用父类的repaint()方法进行重画
+                try {
+                    Thread.sleep(100);    //该线程每隔100毫秒启动一次
+                }catch (InterruptedException e){
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+
+
 }
